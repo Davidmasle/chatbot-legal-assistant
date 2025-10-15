e0fb7dcb21096c6eaedbbd44cb32784e403a70b2
Davidmasle <dav1234529@gmail.com>
2025-10-16 02:10:48 +0300

    clear tmp-files

--- create_docx.py
+++
@@ -1,8 +0,0 @@
-from docx import Document
-
-doc = Document()
-doc.add_paragraph("Test docx file for adada  Legal Assistant")
-doc.add_paragraph("Example text should be divided in chunks addadasd a dasdasdasd dadad")
-doc.save("sample.docx")
-
-print("DOCX created: sample.docx")
